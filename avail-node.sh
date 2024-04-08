33ccb349d91ad6230cbe3df97329215a2618f0cb
Panacea <30661108+Peeliye@users.noreply.github.com>
2024-04-09 04:50:11 +0800

    Update avail-node.sh

--- avail-node.sh
+++ avail-node.sh
@@ -10,23 +10,3 @@ EOF
 
 # 使用screen运行curl命令
 screen -dmS avail_session bash -c "curl -sL https://avail.sh | bash -s -- --identity identity.toml"
-
-# 配置 systemd 服务文件
-sudo tee /etc/systemd/system/availd.service > /dev/null << EOF
-[Unit]
-Description=Avail Light Client
-After=network.target
-StartLimitIntervalSec=0
-[Service]
-User=root
-ExecStart=/root/.avail/bin/avail-light --network goldberg --identity /root/identity.toml
-Restart=always
-RestartSec=120
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# 重新加载 systemd 并启用并启动服务
-sudo systemctl daemon-reload
-sudo systemctl enable availd
-sudo systemctl start availd.service
