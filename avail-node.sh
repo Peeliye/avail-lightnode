344f37cb4da9bdff3945a6a6242a735c75ff0038
Panacea <30661108+Peeliye@users.noreply.github.com>
2024-04-09 06:09:40 +0800

    kill old service

--- avail-node.sh
+++ avail-node.sh
@@ -2,7 +2,14 @@
 
 rm -rf ./*
 
-systemctl stop availd.service
+# 检查服务是否存在并且正在运行
+if systemctl is-active --quiet availd.service; then
+    echo "Service is active, stopping..."
+    sudo systemctl stop availd.service
+    echo "Service has been stopped."
+else
+    echo "Service is not active or does not exist, no action taken."
+fi
 
 # 用户输入秘密种子短语
 read -p "12/24位助词: " avail_secret_seed_phrase
